21eee69f74c5adf2d14bd37f08b63c0738f2b058
Ved Shrimali <vedshrimali123@gmail.com>
2026-06-23 16:42:54 +0530

    Update production API URLs

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -4,3 +4,11 @@ const CONFIG = {
     TOKEN_KEY: 'jwt_token',
     USER_KEY: 'user_data'
 };
+
+//for aws
+const CONFIG = {
+    AUTH_API: 'http://54.152.207.94:8080',
+    ROUTE_API: 'http://54.152.207.94:8081',
+    TOKEN_KEY: 'jwt_token',
+    USER_KEY: 'user_data'
+};
