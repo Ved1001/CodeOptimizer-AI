928ec1f1c63676ff482fe09311a94d8aacb0d6d9
Ved Shrimali <vedshrimali123@gmail.com>
2026-06-23 17:03:06 +0530

    Local changes

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,9 +1,11 @@
+/*
 const CONFIG = {
     AUTH_API: 'http://localhost:8080',
     ROUTE_API: 'http://localhost:8081',
     TOKEN_KEY: 'jwt_token',
     USER_KEY: 'user_data'
 };
+*/
 
 //for aws
 const CONFIG = {
