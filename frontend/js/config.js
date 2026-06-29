140cbe56a804aafc23149650eb59344bbc694e6a
SHRIMALI VED R <158802410+Ved1001@users.noreply.github.com>
2026-06-29 19:36:05 +0530

    Change API endpoints to production URLs
    Updated CONFIG object for production environment.

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,9 +1,11 @@
+/* for local 
 const CONFIG = {
     AUTH_API: 'http://localhost:8080',
     ROUTE_API: 'http://localhost:8081',
     TOKEN_KEY: 'jwt_token',
     USER_KEY: 'user_data'
 };
+*/
 
 /*for aws
 const CONFIG = {
@@ -13,3 +15,10 @@ const CONFIG = {
     USER_KEY: 'user_data'
 };
 */
+
+const CONFIG = {
+    AUTH_API: 'https://api-gateway-qks5.onrender.com',
+    ROUTE_API: 'https://api-gateway-qks5.onrender.com',
+    TOKEN_KEY: 'jwt_token',
+    USER_KEY: 'user_data'
+};
